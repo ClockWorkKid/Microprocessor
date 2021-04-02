6311e0a94e465eb3509760765ab265514c4c07b9
Mir Sayeed Mohammad <58524430+ClockWorkKid@users.noreply.github.com>
2021-04-02 11:49:05 +0600

    Lab week 4

--- Microprocessor Laboratory/Exp 4 - Program Stack and Arrays/code-410 Report 2 Division by shifting.asm
+++ Microprocessor Laboratory/Exp 4 - Program Stack and Arrays/code-410 Report 2 Division by shifting.asm
@@ -1,4 +1,5 @@
 ; Perform division operation by shifting
+; https://stackoverflow.com/questions/12697523/performing-bit-division-without-arithmetic-operators/12699549#12699549
 
 CODE    SEGMENT
     ASSUME CS:CODE, DS:CODE
@@ -12,23 +13,67 @@ CODE    SEGMENT
     
     ; *************************** DATA *************************** ;
     
-    x       DW 13     ; dividend
-    y       DW 3      ; divisor   
+    x       DW 20     ; dividend
+    y       DW 7      ; divisor   
     q       DW 0      ; quotient
     r       DW 0      ; remainder
-    
+                                  
+    i       DW 8      ; loop counter                       
+                           
     ; ************************ PROCEDURES ************************ ;
      
     ; NOT YET IMPLEMENTED
     
     DIVIDE  PROC      ; q, r : x/y
         
-        MOV AX, x
-        MOV BX, y
-        DIV BX
+                
+        MOV CX, i     ; loop to be executed 8 times
         
-        MOV q, AX
-        MOV r, DX
+        WHILE:
+        
+            PUSH CX
+            DEC  CX
+            MOV  i, CX
+            
+            MOV AX, x
+            MOV BX, y
+            MOV CX, q
+            MOV DX, r
+            
+            SHL CX, 1           ; quotient <<= 1
+            SHL DX, 1           ; remainder <<= 1
+            
+            MOV q, CX
+            MOV r, DX
+            
+            MOV CX, i
+            
+            MOV DX, 1
+            SHL DX, CL          ; 1<<i
+            
+            AND AX, DX          ; dividend & [1<<i]
+            SHR AX, CL          ; [dividend & [1<<i]] >> i
+            
+            MOV DX, r
+            OR  DX, AX          ; remainder |= [dividend & [1<<i]] >> i 
+            MOV r, DX
+            
+            CMP DX, BX          ; if remainder >= divisor
+            JS continue
+            
+                SUB DX, BX
+                MOV r, DX       ; remainder = remainder - divisor
+                
+                MOV CX, q
+                OR  CX, 1
+                MOV q, CX
+            
+            
+            continue:
+            
+            POP CX
+            LOOP WHILE
+        
                 
         RET      
 
